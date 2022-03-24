6417b134f950e903d166e4b7d6d9ab3222902035
Zachary Bell <zacharybell@live.com>
2022-03-23 23:14:58 -0500

    Add finished QR Code Component using Tailwind CSS

--- qr_code_component/tailwind.config.js
+++ qr_code_component/tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
+    colors: {
+      'white': 'hsl(0, 0%, 100%)',
+      'light-gray': 'hsl(212, 45%, 89%)',
+      'grayish-blue': 'hsl(220, 15%, 55%)',
+      'dark-blue': 'hsl(218, 44%, 22%)'
+    },
+    fontFamily: {
+      sans: ['Outfit', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
